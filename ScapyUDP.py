bf911e73ba72306c7665e92f0616ed1eb2bb64aa
Deivison-Costaa <139662466+Deivison-Costaa@users.noreply.github.com>
2024-10-26 02:13:10 -0300

    Primeira parte do projeto (falta deixar o código legivel, e a segunda parte onde tô testando

--- ScapyUDP.py
+++ ScapyUDP.py
@@ -1,10 +1,44 @@
-from socket import*
+#from socket import*
 import random
 import struct
+from scapy.all import *
 
-addr = ("15.228.191.109", 50000)
+ipServer = "15.228.191.109"
+dport = 50000
+sport = 50001
+mac_dest = "e0:e8:e6:d8:64:0d"
 
-client_socket = socket(AF_INET, SOCK_DGRAM) 
+#client_socket = socket(AF_INET, SOCK_DGRAM)
+
+def calcular_checksum(data):
+    """
+    Calcula o checksum de 16 bits para a sequência de bytes fornecida com wraparound.
+
+    Args:
+        data (bytes): Dados para os quais o checksum deve ser calculado.
+
+    Returns:
+        int: Valor do checksum em formato numérico.
+    """
+    soma = 0  # Inicializa a soma
+    n = len(data)  # Comprimento dos dados
+
+    # Soma bytes em pares
+    for i in range(0, n, 2):
+        # Pega dois bytes, o segundo byte é deslocado 8 bits para a esquerda
+        if i + 1 < n:
+            par = data[i] + (data[i + 1] << 8)  # Combina dois bytes
+        else:
+            par = data[i]  # Caso haja um byte solitário
+        
+        soma += par  # Adiciona à soma
+
+        # Aplica o wraparound
+        while soma > 0xFFFF:
+            soma = (soma & 0xFFFF) + 1  # Adiciona 1 ao excesso
+
+    # Retorna o complemento
+    return ~soma & 0xFFFF  # Complemento de 1
 
 def criar_mensagem(tipo, identificador_resposta):
     mensagem = (0b0000 << 20) | (tipo << 16) | identificador_resposta # Criando a mensagem de 24 bits
@@ -14,11 +48,17 @@ def criar_mensagem(tipo, identificador_resposta):
 def enviar_mensagem(tipo):
     identificador_mensagem = random.randint(1, 65535)
     mensagem = criar_mensagem(tipo, identificador_mensagem)
-    client_socket.sendto(mensagem, addr)
-
-    resposta, _ = client_socket.recvfrom(1024)
     
-    resposta_mensagem(resposta, identificador_mensagem)
+    #client_socket.sendto(mensagem, addr)
+    # pacote_udp = UDP(sport=sport, dport=dport)
+    # pacote_ip = IP(dst=ipServer)
+    #resposta, _ = client_socket.recvfrom(1024)
+    pacote_completo = IP(src="150.165.134.68", dst=ipServer, proto = 17) / UDP(sport=sport, dport=dport, len = len(mensagem) + 8) / mensagem
+    pacote_completo.show()
+    send(pacote_completo)
+    sr1(pacote_completo, timeout=5)
+    
+    #resposta_mensagem(resposta, identificador_mensagem)
 
 
 def resposta_mensagem(mensagem, identificador_mensagem):
@@ -70,4 +110,4 @@ def cliente():
             
 if __name__ == "__main__":
     cliente()
-    client_socket.close()
+    #client_socket.close()
