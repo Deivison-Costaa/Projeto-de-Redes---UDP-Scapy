6a735a61f24041f339505054e6f069c2ba2b1e6e
AllejandroSousa <allejandro.empresarial@gmail.com>
2024-10-26 03:38:16 +0000

    Reestruturandoo código

--- SocketUDP.py
+++ SocketUDP.py
@@ -1,73 +1,61 @@
-from socket import*
+from socket import socket, AF_INET, SOCK_DGRAM
 import random
 import struct
 
-addr = ("15.228.191.109", 50000)
+SERVER_ADDR = ("15.228.191.109", 50000)
+client_socket = socket(AF_INET, SOCK_DGRAM)
 
-client_socket = socket(AF_INET, SOCK_DGRAM) 
-
-def criar_mensagem(tipo, identificador_resposta):
-    mensagem = (0b0000 << 20) | (tipo << 16) | identificador_resposta # Criando a mensagem de 24 bits
-    # 4 bits para o req/res, 4 bits para o tipo e 16 bits para o identificador
-    return struct.pack('>I', mensagem)[1:] # Removendo o primeiro byte
+def criar_mensagem(tipo, identificador):
+    """Cria uma mensagem de 24 bits com 4 bits para identificar se é de requisição ou resposta, 
+    4 bits indicando o tipo de requisição ou resposta e 16 bits para o identificador"""
+    mensagem = (0b0000 << 20) | (tipo << 16) | identificador
+    return struct.pack('>I', mensagem)[1:]  # Descarte do primeiro byte
 
 def enviar_mensagem(tipo):
-    identificador_mensagem = random.randint(1, 65535)
-    mensagem = criar_mensagem(tipo, identificador_mensagem)
-    client_socket.sendto(mensagem, addr)
+    """Gera identificador aleatório, envia mensagem para o servidor e aguarda resposta"""
+    identificador = random.randint(1, 65535)
+    mensagem = criar_mensagem(tipo, identificador)
+    client_socket.sendto(mensagem, SERVER_ADDR)
 
     resposta, _ = client_socket.recvfrom(1024)
-    
-    resposta_mensagem(resposta, identificador_mensagem)
+    processar_resposta(resposta, identificador)
 
-
-def resposta_mensagem(mensagem, identificador_mensagem):
-    cabecalho = struct.unpack('>3B', mensagem[:3]) # 3 bytes do cabeçalho
-    tipo_resposta = (cabecalho[0] & 0b00001111) # and bit a bit para pegar apenas os 4 bits referentes ao tipo
-    identificador_resposta = (cabecalho[1] << 8) | cabecalho[2]  # Identificador de 2 bytes
+def processar_resposta(mensagem, identificador_esperado):
+    """Descompacta e valida a resposta, exibindo seu conteúdo se o identificador for correspondente"""
+    cabecalho = struct.unpack('>3B', mensagem[:3])  # Extração do cabeçalho
+    tipo_resposta = cabecalho[0] & 0b00001111
+    identificador_resposta = (cabecalho[1] << 8) | cabecalho[2]
     tamanho_resposta = mensagem[3]
-    
-    if identificador_mensagem == identificador_resposta: # verificação para garantia de resposta a mensagem correta
-        match tipo_resposta:
-            case 0:
-                conteudo_resposta = mensagem[4:4 + tamanho_resposta].decode()
-                print(conteudo_resposta)
-            case 1:
-                conteudo_resposta = mensagem[4:4 + tamanho_resposta].decode()
-                print(conteudo_resposta)
-            case 2:
-                conteudo_resposta = mensagem[4:4 + tamanho_resposta]
-                print(int.from_bytes(conteudo_resposta, byteorder='big'))
-            case _:
-                print("Resposta inválida")
-    
+
+    if identificador_resposta == identificador_esperado:
+        conteudo_resposta = mensagem[4:4 + tamanho_resposta]
+        if tipo_resposta == 0 or tipo_resposta == 1:
+            print(conteudo_resposta.decode())
+        elif tipo_resposta == 2:
+            print(int.from_bytes(conteudo_resposta, byteorder='big'))
+        else:
+            print("Resposta inválida")
 
 def cliente():
+    """Interface do cliente para solicitar tipos de requisições e enviar para o servidor"""
+    opcoes = {
+        '1': 0b0000,
+        '2': 0b0001,
+        '3': 0b0010,
+    }
 
     while True:
-        print("\nEscolha um tipo de requisição (Digite o número correspondente):\n\n")
-        print("1. Data e hora atual")
-        print("2. Uma mensagem motivacional para o fim do semestre;")
-        print("3. A quantidade de respostas emitidas pelo servidor até o momento.")
-        print("4. Sair.")
-        print("\n\n")
+        print("\nEscolha uma requisição:\n1. Data e hora atual\n2. Mensagem motivacional\n3. Contagem de respostas\n4. Sair\n")
+        escolha = input("Digite o número da requisição: ")
 
-        num = input("Digite o número da requisição: ")
-        print(f"valor recebido {num}")
-        
-        if num == '1':
-            enviar_mensagem(0b0000)
-        elif num == '2':
-            enviar_mensagem(0b0001)
-        elif num == '3':
-            enviar_mensagem(0b0010)
-        elif num == '4':
+        if escolha in opcoes:
+            enviar_mensagem(opcoes[escolha])
+        elif escolha == '4':
             print("Saindo...")
             break
         else:
-            print("Opção inválida")
-            
-            
+            print("Opção inválida, tente novamente")
+
 if __name__ == "__main__":
     cliente()
     client_socket.close()
